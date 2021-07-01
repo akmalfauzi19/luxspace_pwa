1ebdd05f03dcf8c5700d0a6f74a4df76abad0248
akmal fauzi <akmal.fauzi19@gmail.com>
2021-07-02 00:00:20 +0700

    update

--- src/App.js
+++ src/App.js
@@ -2,14 +2,23 @@ import React from "react";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Browse from "./components/Browse";
+import Arrived from "./components/Arrived";
+import Clients from "./components/Clients";
+import AsideMenu from "./components/AsideMenu";
+import Footer from "./components/Footer";
+
 
 
 function App() {
   return (
    <>
-    <Header />
-     <Hero />
-     <Browse />
+      <Header />
+      <Hero />
+      <Browse />
+      <Arrived />
+      <Clients />
+      <AsideMenu />
+      <Footer />
    </>
   );
 }
